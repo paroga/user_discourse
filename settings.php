ea3a6e820fbdacb4dca4f6d81f8f20e6a95f93f2
Patrick Gansterer <paroga@paroga.com>
2020-03-01 13:12:06 +0100

    Compatibility with version 18

--- settings.php
+++ settings.php
@@ -3,8 +3,9 @@
 OCP\User::checkAdminUser();
 OCP\Util::addScript('user_discourse', 'settings');
 OCP\Util::addStyle('user_discourse', 'settings');
+$config = OC::$server->getConfig();
 $tmpl = new OCP\Template('user_discourse', 'settings');
-$tmpl->assign( 'discourse_api_key', OCP\Config::getAppValue('user_discourse', 'discourse_api_key', ''));
-$tmpl->assign( 'discourse_url', OCP\Config::getAppValue('user_discourse', 'discourse_url', ''));
-$tmpl->assign( 'discourse_sso_secret', OCP\Config::getAppValue('user_discourse', 'discourse_sso_secret', ''));
+$tmpl->assign( 'discourse_api_key', $config->getAppValue('user_discourse', 'discourse_api_key', ''));
+$tmpl->assign( 'discourse_url', $config->getAppValue('user_discourse', 'discourse_url', ''));
+$tmpl->assign( 'discourse_sso_secret', $config->getAppValue('user_discourse', 'discourse_sso_secret', ''));
 return $tmpl->fetchPage();
