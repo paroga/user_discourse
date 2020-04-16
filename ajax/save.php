e9958b412211915cb1548813df7a2582af370cee
Patrick Gansterer <paroga@paroga.com>
2020-04-17 00:22:06 +0200

    Fix settings page

--- ajax/save.php
+++ ajax/save.php
@@ -3,13 +3,13 @@
 OCP\User::checkAdminUser();
 
 // CSRF check
-OCP\JSON::callCheck();
+\OC_JSON::callCheck();
 
 $ok = true;
 $params = array('discourse_api_key', 'discourse_url', 'discourse_sso_secret');
 foreach($params as $param) {
   if (isset($_POST[$param]))
-    OCP\Config::setAppValue('user_discourse', $param, $_POST[$param]);
+    \OC::$server->getConfig()->setAppValue('user_discourse', $param, $_POST[$param]);
 }
 
-OCP\JSON::success(array("data" => array()));
+\OC_JSON::success(array("data" => array()));
